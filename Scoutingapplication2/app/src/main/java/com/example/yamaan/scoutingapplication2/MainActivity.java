5cd799363d041ef8d246baf5afeb2d242e6348f0
yamaan93 <yamaan93@gmail.com>
2018-08-01 22:59:08 -0400

    cant figure out how check box's work

--- Scoutingapplication2/app/src/main/java/com/example/yamaan/scoutingapplication2/MainActivity.java
+++ Scoutingapplication2/app/src/main/java/com/example/yamaan/scoutingapplication2/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ArrayList Match_values =new ArrayList();//Creating arraylist.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onButtonTap(View v){
-        System.out.println("does i do andriod now?");
+        setContentView(R.layout.auto);
+    }
+    public void autoline(View v){
+
     }
 }
