fa4ac15360e254334f8cec2e02a384275786c947
yamaan93 <yamaan93@gmail.com>
2018-08-02 21:07:08 -0400

    colin look, im a big boi now

--- Scoutingapplication2/app/src/main/java/com/example/yamaan/scoutingapplication2/MainActivity.java
+++ Scoutingapplication2/app/src/main/java/com/example/yamaan/scoutingapplication2/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.yamaan.scoutingapplication2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList Match_values =new ArrayList();//Creating arraylist.
+    Boolean autoline,highGoal, lowGoal = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +17,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onButtonTap(View v){
-        // starts new scouting match 
+        // starts new scouting match
         setContentView(R.layout.auto);
+    }
+    public String readNumber(View v){
+        TextView teamNumber;
+        teamNumber = (TextView) findViewById(R.id.team_numbers);
+        System.out.println(teamNumber.getText().toString());
+        return  teamNumber.getText().toString();
     }
     public void autoline(View v){
 
     }
+    public void lowGoal(View v){
+
+    }
+    public void highGoal(View v){
+
+    }
+    public void testVal (View v){
+
+    }
+    public void teleOp(View v){
+        // set teleop scouting area
+
+    }
+    public void test (View v){
+        System.out.println("haha ");
+    }
+
 }
